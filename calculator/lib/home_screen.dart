9bd783d778cb455d6058f29c05df552a1207be5e
zains dube <mwiindezains@gmail.com>
2023-07-25 07:13:53 +0200

    first commit

---
+++ calculator/lib/home_screen.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Calculator'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      equation,
+                      style: TextStyle(
+                          fontSize: 40.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      result,
+                      style: TextStyle(
+                          fontSize: 50.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(child: Divider()),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    createButton('7'),
+                    createButton('8'),
+                    createButton('9'),
+                    createButton('/'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    createButton('4'),
+                    createButton('5'),
+                    createButton('6'),
+                    createButton('*'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    createButton('1'),
+                    createButton('2'),
+                    createButton('3'),
+                    createButton('+'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    createButton('0'),
+                    createButton('.'),
+                    createButton('00'),
+                    createButton('-'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    createButton('CLEAR'),
+                    createButton('DELETE'),
+                    createButton('='),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  String equation = '0';
+  String result = '0';
+  String useful = '';
+
+  buttonPressed(String myButton) {
+    setState(() {
+      if (myButton == 'CLEAR') {
+        equation = '0';
+        result = '0';
+      } else if (myButton == 'DELETE') {
+        equation = equation.substring(0, equation.length - 1);
+        if (equation == '') {
+          equation = '0';
+        }
+      } else if (myButton == '=') {
+        try {
+          equation = useful;
+        } catch (e) {
+          result = 'Syntax Error';
+        }
+      } else {
+        if (equation == '0') {
+          equation = myButton;
+        } else {
+          equation = equation + myButton;
+        }
+      }
+    });
+  }
+
+  //widget for building calculator buttons
+  Widget createButton(String myButton) {
+    return Expanded(
+        child: SizedBox(
+      height: 50.0,
+      child: Padding(
+        padding: EdgeInsets.all(5.0),
+        child: OutlinedButton(
+          onPressed: () {
+            buttonPressed(myButton);
+          },
+          child: Text(
+            myButton,
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+      ),
+    ));
+  }
+}
---
+++ calculator/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:calculator/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+      theme: ThemeData.dark(),
+    );
+  }
+}
